30147191810486088cfef85f68d8cae3273603a0
Dimas2106 <dharmadimas12@gmail.com>
2021-09-20 19:03:31 +0700

    upload awal project

---
+++ application/controllers/Dimascontoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Dimascontoh1 CI_controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "Nama saya Dimas Dharma Yudistira saya tinggal di daerah Kuala Dua olahraga yang saya sukai adalah Bulutangkis"
+    }
+}
