cc61ed9347beb64ea5e967955efae3e8791e7d11
Dimas2106 <dharmadimas12@gmail.com>
2021-09-24 15:34:10 +0700

    p

---
+++ application/views/view-latihan.php
@@ -0,0 +1,14 @@
+<html>
+    
+<head>
+    <title>Latihan1</title>
+</head>
+
+<body>
+    Halo kawan, yuk kita belajar Web Programming..!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
