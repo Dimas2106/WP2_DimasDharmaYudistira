4c7d64c2912e81284895193eccf0f9602621dbea
Dimas2106 <dharmadimas12@gmail.com>
2021-11-27 13:55:04 +0700

    tambahan pertemuan 11

--- application/views/buku/index.php
+++ application/views/buku/index.php
@@ -1,15 +1,5 @@
 <!-- Begin Page Content -->
-
 <div class="container-fluid">
-
-    <?= $this->session->flashdata('pesan'); ?>
-    <div class="row">
-        <div class="col-lg-3">
-            <?php if(validation_errors()){?>
-                <div class="alert alert-danger" role="alert">
-                    <?= validation_errors();?>
-                </div>
-            <?php }?>
 
 
 <?= $this->session->flashdata('pesan'); ?>
@@ -46,17 +36,6 @@ $a = 1;
 foreach ($buku as $b) { ?> 
 <tr>
 
-            <?= $this->session->flashdata('pesan'); ?>
-            <a href="" class="btn btn-primary mb-3" datatoggle="modal" data-target="#kategoriBaruModal"><i class="fas fafile-alt"></i> Tambah Kategori</a>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Kategori</th>
-                        <th scope="col">Pilihan</th>
-                    </tr>
-                </thead>
-                <tbody>
 
 
 <th scope="row"><?= $a++; ?></th>
@@ -75,27 +54,6 @@ foreach ($buku as $b) { ?>
 
 type="image/svg+xml">
 
-                    <?php
-                        $a = 1;
-                        foreach ($kategori as $k) { ?>
-                    <tr>
-                        <th scope="row"><?= $a++; ?></th>
-                        <td><?= $k['kategori']; ?></td>
-                        <td>
-                            <a href="<?=
-base_url('buku/ubahBuku/').$k['id'];?>" class="badge badge-info"><i
-class="fas fa-edit"></i> Ubah</a>
-                                <a href="<?=
-base_url('buku/hapusbuku/').$k['id'];?>" onclick="return
-confirm('Kamu yakin akan menghapus <?= $judul.' '.$k['kategori'];?> ?');"class="badge badge-danger"><i class="fas fa-trash"></i>
-Hapus</a>
-                        </td>
-                    </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
 <picture>
 <source srcset=""
 
@@ -128,23 +86,6 @@ fa-trash"></i> Hapus</a>
 </div>
 <!-- End of Main Content -->
 
-<!-- Modal Tambah kategori baru-->
-<div class="modal fade" id="kategoriBaruModal" tabindex="-1"
-role="dialog" aria-labelledby="kategoriBaruModalLabel" ariahidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title"
-id="kategoriBaruModalLabel">Tambah Kategori</h5>
-                <button type="button" class="close" datadismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <form action="<?= base_url('buku/kategori'); ?>"
-method="post">
-                <div class="modal-body">
-                    <div class="form-group">
-                        <select name="kategori" class="form-control
 
 <!-- Modal Tambah buku baru-->
 <div class="modal fade" id="bukuBaruModal" tabindex="-1" role="dialog" aria-labelledby="bukuBaruModalLabel" aria- hidden="true">
@@ -195,29 +136,6 @@ nama penerbit">
 
 
 form-control-user">
-                            <option value="">Pilih Kategori</option>
-                            <?php
-                            $k =
-['Sains','Hobby','Komputer','Komunikasi','Hukum','Agama','Populer','
-Bahasa','Komik'];
-                            for ($i=0;$i<9;$i++) { ?>
-                                <option value="<?= $k[$i];?>"><?=
-$k[$i];?></option>
-                            <?php } ?>
-                        </select>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary"
-data-dismiss="modal"><i class="fas fa-ban"></i> Close</button>
-                    <button type="submit" class="btn btn-primary"><i
-class="fas fa-plus-circle"></i> Tambah</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-<!-- End of Modal Tambah Menu -->
 
 
 
@@ -262,4 +180,4 @@ stok">
 </div>
 </div>
 </div>
-<!-- End of Modal Tambah Mneu -->
+<!-- End of Modal Tambah Mneu --> 
